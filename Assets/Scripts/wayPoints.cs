6a6bbd4332de11e5e0f94a6d4410441711a21863
Mehrdad Naserdoust <mehrdad.naser@gmail.com>
2020-11-29 19:19:46 -0500

    Enemy AI
    Upload Project

--- Assets/Scripts/wayPoints.cs
+++ Assets/Scripts/wayPoints.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class wayPoints : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Transform[] points;
+    private void Awake()
     {
-        
-    }
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
