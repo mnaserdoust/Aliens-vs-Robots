c973c15b35abf33af8e2eac1b12748518d7f7134
Mehrdad Naserdoust <mehrdad.naser@gmail.com>
2020-12-02 07:12:15 -0500

    M10Gun added

---
+++ Assets/Scripts/waveSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class waveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public float timeBetweenWaves = 5f;
+    public Transform spawnPoint;
+
+    public Text waveCounDownText;
+
+    private float countDown = 2f;
+    private int waveIndex = 0;
+    private void Update()
+    {
+        if(countDown <=0f)
+        {
+            StartCoroutine( spawnWave());
+            countDown = timeBetweenWaves;
+        }
+        countDown -= Time.deltaTime;
+        waveCounDownText.text = Mathf.Round( countDown).ToString();
+    }
+
+    IEnumerator spawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            spawnEnemy();
+            yield return new WaitForSeconds(.6f);
+        }
+        
+    }
+
+    void spawnEnemy()
+    {
+        Instantiate(enemyPrefab,spawnPoint.position,spawnPoint.rotation);
+    }
+}
---
+++ Assets/m10Gun.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class m10Gun : MonoBehaviour
+{
+    private Transform target;
+    public float range = 15f;
+    public string enemyTag = "Enemy";
+    public float turnspeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("updateTarget", 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion look = Quaternion.LookRotation(dir);
+        Vector3 rotation =Quaternion.Lerp(transform.rotation,look,Time.deltaTime *turnspeed).eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+    void updateTarget()
+    {
+        
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestdestination = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance < shortestdestination)
+            {
+                shortestdestination = distance;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestdestination <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+}
